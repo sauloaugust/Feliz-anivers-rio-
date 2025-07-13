ea24e8ade6945012683fbcbb52ed5a0bb25fce7c
sauloaugusto <127687589+sauloaugust@users.noreply.github.com>
2025-07-13 00:59:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ function downloadVideo() {
   a.download = "video.mp4";
   a.click();
 }
+
+window.addEventListener("load", () => {
+  const video = document.getElementById("video-bg");
+  video.play().catch((e) => {
+    console.warn("Vídeo não pode ser iniciado automaticamente:", e);
+  });
+});
